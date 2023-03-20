bf3619bdb8b79406f4def9a764bf2a223be5538f
daniel <65409937+DanielPinheirox@users.noreply.github.com>
2023-03-19 21:20:28 -0300

    primeiro commit

---
+++ cypress/integration/1-getting-started/alura-busca.spec.js
@@ -0,0 +1,20 @@
+describe('alura busca cursos', () => {
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+    });
+
+    beforeEach(() => {
+        cy.visit('https://www.alura.com.br');
+    })
+    it('buscar curso de java', () =>{
+        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('.header-barraBusca-form-submit').click();
+        //identificação por seleção
+        //cy.get(':nth-child(5) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
+        //.should('have.text', 'Formação Java e Orientação a Objetos');
+        //identificação por ID
+        cy.get('h4.busca-resultado-nome').should('contain.text', 'Formação Aprenda a programar em Java com Orientação a Objetos');
+        //comentario da primeira aula
+    })
+})
